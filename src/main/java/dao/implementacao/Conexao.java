775a56abade129e59b22c06ce67e307a93f34011
Ralph <Ralph@192.168.0.31>
2017-11-01 15:07:51 -0200

    #atualização do pom.xml

--- src/main/java/dao/implementacao/Conexao.java
+++ src/main/java/dao/implementacao/Conexao.java
@@ -6,14 +6,18 @@ import java.sql.SQLException;
 
 public class Conexao {
 	
-	static private final String url = "jdbc:oracle:thin:@localhost:1521:XE";
-	static private final String usuario = "Pilares_tg";
-	static private final String senha = "123";
+//	static private final String url = "jdbc:oracle:thin:@localhost:1521:XE";
+//	static private final String usuario = "Pilares_tg";
+//	static private final String senha = "123";
+	static private final String url = "jdbc:mysql:sql10.freemysqlhosting.net:3306/sql10202547";
+	static private final String usuario = "sql10202547";
+	static private final String senha = "nGVuKYDml9";
     static public Connection conexao;
 	
 	static public void conectar(){
 		try {
-			Class.forName("oracle.jdbc.OracleDriver");
+//			Class.forName("oracle.jdbc.OracleDriver");
+			Class.forName("com.mysql.jdbc.Driver"); 
 			conexao = DriverManager.getConnection(url, usuario, senha);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
